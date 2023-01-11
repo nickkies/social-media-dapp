5d9c08915b7ea0905d7b9c22b756e8af37f01e2f
YoonSung Kim <opqudk@gmail.com>
2023-01-11 17:31:59 +0900

    feat: "add_post"

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{AccountId, near_bindgen};
+use near_sdk::{AccountId, near_bindgen, env};
 use near_sdk::serde::{Serialize, Deserialize};
 use near_sdk::collections::{UnorderedMap};
 
@@ -33,5 +33,37 @@ impl Default for SocialNetworking {
             likes_by_user_id: UnorderedMap::new(b'n'),
             posts_by_tag: UnorderedMap::new(b'o')
         }
+    }
+}
+
+#[near_bindgen]
+impl SocialNetworking {
+    pub fn add_post(
+        &mut self, 
+        title: String, 
+        description: String, 
+        tags: String, 
+        media: String
+    ) -> Post {
+        let  tags_iterator = tags.split(",");
+        let mut tags = Vec::<String>::new();
+        for tag in tags_iterator {
+            tags.push(tag.to_string());
+        }
+
+        let post = Post {
+            id: self.number_of_posts,
+            title,
+            description,
+            tags: tags.clone(),
+            media,
+            user_who_liked: Vec::<AccountId>::new(),
+            owner_id: env::signer_account_id()
+        };
+
+        self.posts.insert(&post.id, &post);
+        self.number_of_posts += 1;
+        
+        post
     }
 }
