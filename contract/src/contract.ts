a1ccb0525f85582011b7e5f6501bddf8c64cc7a1
YoonSung Kim <opqudk@gmail.com>
2023-01-30 15:11:56 +0900

    feat: "Add social media class"

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,4 +1,16 @@
-import { NearBindgen, near, call, view } from 'near-sdk-js';
+import { NearBindgen, UnorderedMap } from 'near-sdk-js';
 
 @NearBindgen({})
-class HelloNear {}
+class SocialMedia {
+  posts: UnorderedMap;
+  number_of_posts: number;
+  likes_by_user_id: UnorderedMap;
+  posts_by_tag: UnorderedMap;
+
+  constructor() {
+    this.posts = new UnorderedMap('p');
+    this.number_of_posts = 0;
+    this.likes_by_user_id = new UnorderedMap('l');
+    this.posts_by_tag = new UnorderedMap('t');
+  }
+}
